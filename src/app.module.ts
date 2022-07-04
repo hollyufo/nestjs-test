762336724e212bd7834e41d2c092b60cf433ca81
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-07-04 13:22:54 +0100

    redone everything

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
-import { CourseModule } from './course/course.module';
 import { ProfessorModule } from './professor/professor.module';
-
+import { CourseModule } from './course/course.module';
+import { Course } from './course/entities/course.entity';
+import { Professor } from './professor/entities/professor.entity';
+import { Student } from './student/entities/student.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [StudentModule, CourseModule, ProfessorModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      database: 'course-management',
+      username: 'root',
+      password: '',
+      synchronize: true,
+      entities: [Professor, Course, Student],
+    }),
+    CourseModule,
+    ProfessorModule,
+    StudentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
